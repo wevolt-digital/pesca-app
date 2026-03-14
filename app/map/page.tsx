67dd8766a1096845e3b0ef87191902b86cf922bd
wevolt-digital <wevolt-digital@gmail.com>
2026-03-13 23:17:33 -0300

    Remove botão de adicionar nos controles do mapa

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -293,7 +293,6 @@ export default function MapPage() {
       <FloatingMapControls
         onRecenter={handleRecenter}
         onFilter={() => setShowFilterPanel(!showFilterPanel)}
-        onAdd={() => {}}
       />
 
       {selectedMarker && (
